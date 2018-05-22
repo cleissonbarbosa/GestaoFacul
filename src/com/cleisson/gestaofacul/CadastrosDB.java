a8c8884defaff09f5b35414a7a32f8b6c0670e44
cleisson Barbosa <cleisson147@gmail.com>
2018-05-22 12:05:50 -0300

    Em Desenvolvimento

---
+++ src/com/cleisson/gestaofacul/CadastrosDB.java
@@ -0,0 +1,88 @@
+package com.cleisson.gestaofacul;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cleisson
+ */
+public class CadastrosDB {
+    
+    private int dataDeAdimissao;
+    private String endereco;
+    private int matricula;
+    private String nome;
+    private float salario;
+    private int telefone;
+    private String escolaOuSetor;
+
+    public CadastrosDB(int dataDeAdimissao, String endereco, int matricula, String nome, float salario, int telefone, String escolaOuSetor) {
+        this.dataDeAdimissao = dataDeAdimissao;
+        this.endereco = endereco;
+        this.matricula = matricula;
+        this.nome = nome;
+        this.salario = salario;
+        this.telefone = telefone;
+        this.escolaOuSetor = escolaOuSetor;
+    }
+
+    public String getEscolaOuSetor() {
+        return escolaOuSetor;
+    }
+
+    public void setEscolaOuSetor(String escolaOuSetor) {
+        this.escolaOuSetor = escolaOuSetor;
+    }
+    
+    
+    public int getDataDeAdimissao() {
+        return dataDeAdimissao;
+    }
+
+    public void setDataDeAdimissao(int dataDeAdimissao) {
+        this.dataDeAdimissao = dataDeAdimissao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+}
---
+++ src/com/cleisson/gestaofacul/Escolas.java
@@ -0,0 +1,24 @@
+package com.cleisson.gestaofacul;
+
+/**
+ *
+ * @author cleisson
+ */
+public class Escolas {
+    private String nomeEscolas;
+
+    public Escolas(String Nome) {
+        this.nomeEscolas = Nome;
+    }
+
+    
+    public String getNome() {
+        return nomeEscolas;
+    }
+
+    public void setNome(String Nome) {
+        this.nomeEscolas = Nome;
+    }
+    
+    
+}
